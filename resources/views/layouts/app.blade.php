14673584d604fe2f5645846ea40887d0235fca7c
thavarshan <tjthavarshan@gmail.com>
2020-06-12 22:35:52 +0530

    Update site title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PHP Katas') }} | with PHPUnit</title>
+    <title>{{ config('app.name', 'CodeKatas') }} With PHPUnit</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
